7fb9f6773eb25ea81759026e12e200f894e944e4
aston4ever <aston4ever@gmail.com>
2024-02-05 15:12:25 +0300

    feat: add functions and log

--- 08_1 Hoisting/script.js
+++ 08_1 Hoisting/script.js
@@ -1,1 +1,22 @@
 'use strict';
+console.log(me)
+// console.log(job)
+// console.log(year)
+
+var me = 'Aston';
+let job = 'teacher';
+const year = 1991;
+
+console.log(add(2,3))
+console.log(addExpr(2,3))
+console.log(arrow(2,3))
+
+function add(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const arrow = (a, b)=> a + b;
