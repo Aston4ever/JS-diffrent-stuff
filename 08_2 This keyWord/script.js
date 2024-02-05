c2688e5ec9e4b6659dc48c54b2f9459e3385d047
aston4ever <aston4ever@gmail.com>
2024-02-05 15:57:46 +0300

    feat: add log this arrow func

--- 08_2 This keyWord/script.js
+++ 08_2 This keyWord/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+let obj = {
+    name: 'Aston',
+    showName: function () {
+        let arr = ()=> {
+            console.log(this.name)
+        }
+        arr();
+    }
+}
+let arr2 = () => {
+    console.log(this)
+}
+arr2();
+obj.showName();
